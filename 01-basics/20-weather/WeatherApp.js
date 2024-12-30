d23e22212e20362e316ad8e8d3f7290591a37795
alnusglutinosa <olga.app2@gmail.com>
2024-12-30 17:57:56 +0200

    20-weather

--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -1,47 +1,103 @@
 import { defineComponent } from 'vue'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
+function timeToMinutes(time) {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+function isNightTheme(dt, sunset, sunrise) {
+  const currentTimeInMinutes = timeToMinutes(dt);
+  const sunsetTimeInMinutes = timeToMinutes(sunset);
+  const sunriseTimeInMinutes = timeToMinutes(sunrise);
+
+  return currentTimeInMinutes > sunsetTimeInMinutes
+    || currentTimeInMinutes < sunriseTimeInMinutes ;
+}
+
+const weatherData = getWeatherData().map(item => {
+  return {
+    ...item,
+    icon: WeatherConditionIcons[item.current.weather.id],
+    iconTitle: item.current.weather.description,
+    pressureMmHg: (item.current.pressure * 0.75).toFixed(0),
+    nightTheme: isNightTheme(item.current.dt, item.current.sunset, item.current.sunrise),
+    tempCelsius: (item.current.temp - 273.15).toFixed(1),
+  };
+});
+
+
 export default defineComponent({
   name: 'WeatherApp',
+  setup() {
+    return {
+      weatherData,
+    }
+  },
+
 
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
 
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
+        <li
+          v-for="item in weatherData"
+          class="weather-card"
+          :class="{'weather-card--night': item.nightTheme }"
+        >
+          <div
+            v-if="item.alert"
+            class="weather-alert"
+          >
             <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
+            <span class="weather-alert__description">
+              {{ item.alert.sender_name }}: {{ item.alert.description }}
+            </span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{ item.geographic_name }}
             </h2>
             <div class="weather-card__time">
-              07:17
+              {{ item.current.dt }}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
+            <div
+              class="weather-conditions__icon"
+              :title="item.iconTitle"
+            >
+              {{ item.icon }}
+            </div>
+            <div class="weather-conditions__temp">
+              {{ item.tempCelsius }} °C
+            </div>
           </div>
           <div class="weather-details">
             <div class="weather-details__item">
               <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
+              <div class="weather-details__item-value">
+                {{ item.pressureMmHg}}
+              </div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value">
+                {{ item.current.humidity }}
+              </div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">
+                {{ item.current.clouds }}
+              </div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">
+                {{ item.current.wind_speed }}
+              </div>
             </div>
           </div>
         </li>
